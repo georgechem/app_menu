c3ed2f6abcddf4bf118449f4e04e30dfefdcb250
root <grzegorzchrymecki@php-developer.ie>
2021-02-18 12:58:32 +0000

    fade-in and fade out working

--- public/js/components/menu/menu.js
+++ public/js/components/menu/menu.js
@@ -3,12 +3,60 @@ const navMenu = document.getElementById('navMenu');
 const navSubMenu = document.getElementById('navSubMenu');
 
 // Menu functions
-function toggleHiddenClass(){
-    navMenu.classList.toggle('hidden');
-    navSubMenu.classList.toggle('hidden');
+function toggleHiddenClass(elements = []){
+    elements.forEach(function(item){
+        item.classList.toggle('hidden');
+    });
+
+}
+// Fadeout function
+function fadeOut(element, toggleClasses = []){
+    let opacity = 1.0;
+    // duration in ms
+    let duration = 500;
+    // desired fps
+    let fps = 60;
+    let fadeOutInterval = setInterval(function(){
+    opacity -= (1/fps);
+    if (opacity <= 0){
+        opacity = 0;
+        clearInterval(fadeOutInterval);
+        if (toggleClasses.length !== 0){
+            toggleHiddenClass(toggleClasses);
+        }
+    }
+
+    element.style.opacity = opacity;
+    },duration/fps);
+}
+// FadeIn function
+function fadeIn(element, toggleClasses = []){
+    let opacity = 0.0;
+    // duration in ms
+    let duration = 1000;
+    // desired fps
+    let fps = 60;
+    let fadeOutInterval = setInterval(function(){
+        opacity += (1/fps);
+        if (opacity >= 1){
+            opacity = 1;
+            clearInterval(fadeOutInterval);
+            if (toggleClasses.length !== 0){
+                toggleHiddenClass(toggleClasses);
+            }
+        }
+
+        element.style.opacity = opacity;
+    },duration/fps);
 }
 
-
 // Menu actions
-navMenu.addEventListener('click', toggleHiddenClass);
-navSubMenu.addEventListener('click', toggleHiddenClass);
+navMenu.addEventListener('click', function(){
+    fadeOut(navMenu, [navMenu, navSubMenu]);
+    fadeIn(navSubMenu);
+});
+navSubMenu.addEventListener('click', function(){
+    fadeOut(navSubMenu, [navMenu, navSubMenu]);
+    fadeIn(navMenu);
+});
+
