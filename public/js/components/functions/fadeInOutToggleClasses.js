e0768d09d8ffa818bfc78c85dadbe94645f5b5ea
root <grzegorzchrymecki@php-developer.ie>
2021-02-18 14:26:54 +0000

    fade-in and fade out working

---
+++ public/js/components/functions/fadeInOutToggleClasses.js
@@ -0,0 +1,47 @@
+// Toggle Classes
+function toggleHiddenClass(elements = []){
+    elements.forEach(function(item){
+        item.classList.toggle('hidden');
+    });
+
+}
+// Fadeout function
+function fadeOut(element, toggleClasses = []){
+    let opacity = 1.0;
+    // duration in ms
+    let duration = 500;
+    // desired fps
+    let fps = 60;
+    let fadeOutInterval = setInterval(function(){
+        opacity -= (1/fps);
+        if (opacity <= 0){
+            opacity = 0;
+            clearInterval(fadeOutInterval);
+            if (toggleClasses.length !== 0){
+                toggleHiddenClass(toggleClasses);
+            }
+        }
+
+        element.style.opacity = opacity;
+    },duration/fps);
+}
+// FadeIn function
+function fadeIn(element, toggleClasses = []){
+    let opacity = 0.0;
+    // duration in ms
+    let duration = 1000;
+    // desired fps
+    let fps = 60;
+    let fadeOutInterval = setInterval(function(){
+        opacity += (1/fps);
+        if (opacity >= 1){
+            opacity = 1;
+            clearInterval(fadeOutInterval);
+            if (toggleClasses.length !== 0){
+                toggleHiddenClass(toggleClasses);
+            }
+        }
+
+        element.style.opacity = opacity;
+    },duration/fps);
+}
--- public/js/components/menu/menu.js
+++ public/js/components/menu/menu.js
@@ -1,54 +1,20 @@
+/**
+ * To work requires first load files
+ * - fadeInOutToggleClasses.js
+ *
+ * @type {HTMLElement}
+ */
+
 // Menu elements
 const navMenu = document.getElementById('navMenu');
 const navSubMenu = document.getElementById('navSubMenu');
 
-// Menu functions
-function toggleHiddenClass(elements = []){
-    elements.forEach(function(item){
-        item.classList.toggle('hidden');
-    });
-
-}
-// Fadeout function
-function fadeOut(element, toggleClasses = []){
-    let opacity = 1.0;
-    // duration in ms
-    let duration = 500;
-    // desired fps
-    let fps = 60;
-    let fadeOutInterval = setInterval(function(){
-    opacity -= (1/fps);
-    if (opacity <= 0){
-        opacity = 0;
-        clearInterval(fadeOutInterval);
-        if (toggleClasses.length !== 0){
-            toggleHiddenClass(toggleClasses);
-        }
+const menuLinks = document.querySelectorAll('.menu__link')
+menuLinks.forEach(function(link){
+    link.onclick = function(e){
+        e.preventDefault();
     }
-
-    element.style.opacity = opacity;
-    },duration/fps);
-}
-// FadeIn function
-function fadeIn(element, toggleClasses = []){
-    let opacity = 0.0;
-    // duration in ms
-    let duration = 1000;
-    // desired fps
-    let fps = 60;
-    let fadeOutInterval = setInterval(function(){
-        opacity += (1/fps);
-        if (opacity >= 1){
-            opacity = 1;
-            clearInterval(fadeOutInterval);
-            if (toggleClasses.length !== 0){
-                toggleHiddenClass(toggleClasses);
-            }
-        }
-
-        element.style.opacity = opacity;
-    },duration/fps);
-}
+} );
 
 // Menu actions
 navMenu.addEventListener('click', function(){
