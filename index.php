b5b9d1c52cd7c1287b86230067a9a1b8b6fb79ae
root <grzegorzchrymecki@php-developer.ie>
2021-02-18 09:14:56 +0000

    ok

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-header ("Location: https://www.etutor.ie/test/test1/public/index.php");
+//header ("Location: https://www.etutor.ie/test/test1/public/index.php");
+header ("Location: public/index.php");
